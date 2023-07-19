3e782af30d7beaeda5fe977fbb71f809b5f74975
JASBIR SINGH <140000756+JasbirSinghRandhawa@users.noreply.github.com>
2023-07-20 01:02:13 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let tablinks = document.getElementsByClassName("tab-links");
 let tabcontents = document.getElementsByClassName("tab-contents");
+let sidemenu=document.getElementById("sidemenu");
 function opentab(tabname) {
   for (let tablink of tablinks) {
     tablink.classList.remove("active-link");
@@ -9,4 +10,12 @@ function opentab(tabname) {
   }
   event.currentTarget.classList.add("active-link");
   document.getElementById(tabname).classList.add("active-tab")
+}
+function openmenu()
+{
+  sidemenu.style.right="0"
+}
+function closemenu()
+{
+  sidemenu.style.right="-200px"
 }
